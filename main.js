8d4d735c2d6f23348863f0115723a89a6f12f080
Aidan Beery <aidanb1049@gmail.com>
2019-11-17 20:23:27 -0800

    Mockup of Ride Length Frequency graphing
    Added chart.js to dependencies 
    Added example line chart for data visualization mockup

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-$(onJoinAlert)
-
-function onJoinAlert(){  
-    alert("Caution: This Application Slays Facists")
+function renderChart(data, labels){
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '2018 Rides',
+                data: data,
+            }]
+        },
+    });
 }
+
+$("#renderBtn").click(
+    function () {
+        data = [20, 14, 12, 15, 18, 19, 22];
+        labels =  ["1 or less", "2", "3", "4", "5", "6", "7","8+"];
+        renderChart(data, labels);
+    }
+);
